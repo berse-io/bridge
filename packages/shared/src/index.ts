13f50c7330d6c39c59aaefec081c1eb949b37471
liamzebedee <liamzebedee@yahoo.com.au>
2019-03-20 12:37:34 +0100

    Added pkg for shared logic

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,11 @@
+export async function zxWeb3Connected(pe, CONNECT_TIMEOUT = 7000) {
+    return new Promise((res, rej) => {
+        pe.on('block', res)
+        setTimeout(
+            _ => {
+                rej(new Error(`Web3.js couldn't connect after ${CONNECT_TIMEOUT}ms`))
+            }, 
+            CONNECT_TIMEOUT
+        )
+    });
+}
