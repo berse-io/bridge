c1fb30dd81effbaa99335e444748697635e4dbe8
liamzebedee <liamzebedee@yahoo.com.au>
2019-03-20 12:37:51 +0100

    Fixed debug silent fail

--- packages/deployer/src/debug.ts
+++ packages/deployer/src/debug.ts
@@ -3,6 +3,9 @@ import { PrivateKeyWalletSubprovider, RPCSubprovider } from "@0x/subproviders";
 import { ConfigManager } from "./config";
 import { Web3Wrapper } from "@0x/web3-wrapper";
 import { fromWei } from "web3-utils";
+import {
+    zxWeb3Connected
+} from '@ohdex/shared';
 
 // checks balances on all networks
 
@@ -16,12 +19,14 @@ import { fromWei } from "web3-utils";
         
         let pe = new Web3ProviderEngine();
         pe.addProvider(new PrivateKeyWalletSubprovider(deploymentAccount.privateKey))
-        pe.addProvider(new RPCSubprovider(config.rpcUrl));
+        pe.addProvider(new RPCSubprovider(config.rpcUrl, 3000));
         pe.start()
 
         let web3 = new Web3Wrapper(pe);
-        let accounts = await web3.getAvailableAddressesAsync();
-        let account = accounts[0];
+        // let accounts = await web3.getAvailableAddressesAsync();
+        // let account = accounts[0];
+        let account = deploymentAccount.address;
+        await zxWeb3Connected(pe)
 
         let balance = await web3.getBalanceInWeiAsync(account)
 
@@ -29,6 +34,4 @@ import { fromWei } from "web3-utils";
 
         pe.stop()
     }
-    
-
-})()
+})().catch(ex => console.error(ex))
