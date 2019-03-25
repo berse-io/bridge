651a580f4ecc5fdd9cddfb6502ab2160b628106f
liamzebedee <liamzebedee@yahoo.com.au>
2019-03-25 15:03:38 +0100

    Fixed old test in contracts/

--- packages/shared/src/contracts.ts
+++ packages/shared/src/contracts.ts
@@ -1,11 +1,25 @@
-import { EventEmitterContract, EventEmitterEvents } from "@ohdex/contracts/lib/build/wrappers/event_emitter";
-import { Web3ProviderEngine, RPCSubprovider } from '0x.js';
+
+import { BridgedTokenContract } from "@ohdex/contracts/build/wrappers/bridged_token";
+import { EscrowContract } from '@ohdex/contracts/build/wrappers/escrow';
+import { EventEmitterContract } from "@ohdex/contracts/build/wrappers/event_emitter";
+import { Web3ProviderEngine, RPCSubprovider, BigNumber } from '0x.js';
+
+export function getContractArtifact(name: string) {
+    let json = require(`@ohdex/contracts/lib/build/artifacts/${name}.json`)
+    return json
+}
 
 export function getContractAbi(name: string) {
     let json = require(`@ohdex/contracts/lib/build/artifacts/${name}.json`)
     return json.compilerOutput.abi;
 }
 
+export function get0xArtifact(name: string) {
+    return require(`@ohdex/contracts/lib/build/artifacts/${name}.json`)
+}
+
+export const _salt = new BigNumber(0);
+export const _chainId = new BigNumber(0);
 
 export class ContractWrappers { 
     static from(networkConf: any, pe: Web3ProviderEngine) {
@@ -14,15 +28,16 @@ export class ContractWrappers {
             networkConf.eventEmitterAddress,
             pe
         )
+
+        const Escrow = new EscrowContract(
+            getContractAbi('Escrow'),
+            networkConf.escrowAddress,
+            pe
+        )
         
         return {
-            EventEmitter
+            EventEmitter,
+            Escrow,
         }
     }
 }
-
-// class EventEmitterWrapper {
-//     constructor(network: string) {
-
-//     }
-// }
