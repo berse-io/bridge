d711771c4ef3094e837f79750b84838e69c069a3
Mick de Graaf <mick@mickdegraaf.nl>
2019-04-03 16:40:35 +0200

    Impl of slave contract (wip)

---
+++ packages/contracts/contracts/master-slave/MasterUtils.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+import './IEvents.sol';
+
+contract MasterUtils is IEvents {
+
+    function getCallHash() public view returns(bytes32) {
+
+    }
+}
---
+++ packages/contracts/contracts/master-slave/Slave.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.5.0;
+
+import '../libs/LibEvent.sol';
+import "../events/EventListener.sol";
+import './IEvents.sol';
+
+contract Slave is IEvents {
+
+    using LibEvent for bytes32;
+
+    address master;
+    uint256 masterChainId;
+
+    mapping(bytes32 => bool) public processed;
+
+    EventListener public eventListener;
+
+    constructor(address _master, uint256 _masterChainId, address _eventListener)  public {
+        master = _master;
+        masterChainId = _masterChainId;
+        eventListener = EventListener(_eventListener);
+    }
+
+
+    function doCall(
+        address payable _to,
+        uint256 _value,
+        bytes memory _data,
+        bytes32[] memory _proof,
+        bool[] memory _proofPaths,
+        bytes32[] memory _eventsProof,
+        bool[] memory _eventsPaths,
+        bytes32 _eventsRoot
+    ) public {
+        // verify event
+        bytes32 eventHash = createCallEvent(_to, _value, _data);
+        eventHash = eventHash.getMarkedEvent(master, masterChainId);
+        require(eventListener.checkEvent(_proof, _proofPaths, _eventsProof, _eventsPaths, _eventsRoot, eventHash), "EVENT_NOT_FOUND");
+        _to.call.value(_value)(_data);
+    } 
+
+}
