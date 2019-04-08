043fd45de299fc82a0ef682417513706ba2f47f1
liamzebedee <liamzebedee@yahoo.com.au>
2019-04-08 23:25:49 +0200

    Added new accounts to geth

--- packages/multichain/geth_dev/init.sh
+++ packages/multichain/geth_dev/init.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-
-echo removing old devChain
-
-rm -rf devChain
-
 echo initialising new blockchain with genesis file
-
 geth --datadir=devChain init genesis.json
-
-echo copying utc file to keystore
-
-cp utc ./devChain/keystore/utc
