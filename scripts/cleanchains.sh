9fc6dba31f9b62c8f42a61c8995a7844dc78af62
liamzebedee <liamzebedee@yahoo.com.au>
2019-04-15 10:48:28 +0200

    Better script

--- scripts/cleanchains.sh
+++ scripts/cleanchains.sh
@@ -1,14 +1,21 @@
 cd packages/multichain
 
+lsof -ti:10000 | xargs kill -9
+lsof -ti:10001 | xargs kill -9
+
+trap 'pkill -P $$' SIGINT SIGTERM EXIT
+
+echo Starting chains
 yarn clean
 yarn start run --chain ethereum --name kovan &
 CHAIN_1=$!
 yarn start run --chain ethereum --name rinkeby &
 CHAIN_2=$!
 
+
 cd ../deployer
-sleep 2
+sleep 3
+echo Deploying
 yarn deploy:testnets
 
-trap 'pkill -P $$' SIGING SIGTERM EXIT
 read
